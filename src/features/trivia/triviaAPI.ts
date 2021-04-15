1e29c5c7201ca44cc6046f97492a17ea9ec0c554
Dan <danko56666@gmail.com>
2021-04-14 22:11:11 -0400

    add rejected case

--- src/features/trivia/triviaAPI.ts
+++ src/features/trivia/triviaAPI.ts
@@ -1,7 +1,6 @@
 import instance from "../../api/index";
 
 const fetchQuestions = async () => {
-  // eslint-disable-next-line no-useless-catch
   try {
     const result = await instance.get(
       "api.php?amount=10&difficulty=hard&type=boolean"
