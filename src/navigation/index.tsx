8df0cdfc901ebb5d3e08434c1d998887015d3e56
Dan <danko56666@gmail.com>
2021-04-17 23:35:10 -0400

    remove theme config

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,20 +1,11 @@
 import React from "react";
-import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 
 import QuizNavigator from "./QuizNavigator";
-import colors from "../ui/theme/colors";
-
-const MyTheme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    background: colors.bg.primary,
-  },
-};
 
 // create container navigation in case app needs authentication or onboarding before proceeding to main app
 const RootStack = (): JSX.Element => (
-  <NavigationContainer theme={MyTheme}>
+  <NavigationContainer>
     <QuizNavigator />
   </NavigationContainer>
 );
